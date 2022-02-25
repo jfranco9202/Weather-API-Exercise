a5e78fb50d8c6c7c515619f8b11866b4f1d5105d
jfranco9202 <jeremy.franco41@yahoo.com>
2022-02-25 08:29:48 -0600

    initial commit

---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.WriteLine("Please enter your API key:");
+            var apiKey = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter your City name: ");
+                var cityName = Console.ReadLine();
+                var weatherURL = $"api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={apiKey}";
+
+                var response = client.GetStringAsync(weatherURL).Result;
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+                Console.WriteLine(response);
+                Console.WriteLine();
+
+            }
+
+
+
+
+
+        }
+    }
+}
